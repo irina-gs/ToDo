61dcd05718fcec0c34f816c31720e132f477969a
irina-gs <guskovai2014@mail.ru>
2023-12-12 10:41:52 +0300

    Fix

--- TodoTests/TodoTests.swift
+++ TodoTests/TodoTests.swift
@@ -11,10 +11,12 @@ import XCTest
 final class TodoTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() throws {
@@ -23,6 +25,7 @@ final class TodoTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertTrue(true)
     }
 
     func testPerformanceExample() throws {
